ca56a8c0297e7cd9e87d95266a48db4ce48785dd
nina8604 <nina.yaremenko86@gmail.com>
2019-05-21 11:18:19 +0300

    adding files

---
+++ Enc.class.php
@@ -0,0 +1,79 @@
+<?php
+// логическое ядро
+class Enc {
+
+	static public function get_keys() {
+		// задаем настройки при создании закрытого ключа, тип ключа OPENSSL_KEYTYPE_RSA, размер в битах будущего ключа (512 бит)
+		$config = array(
+			"private_key_type" => OPENSSL_KEYTYPE_RSA,
+			"private_key_bits" => 512,
+		);
+		// генерируем закрытый ключ и возвращаем его дескриптор
+		$res = openssl_pkey_new( $config );
+		// достаем значение ключа из его дескритпора
+		$privKey = '';
+		// Функция возвращает закрытый ключ в виде строки, строка сохраняется в переменной $privKey,
+		// Первый параметр – это дескриптор ключа
+		openssl_pkey_export( $res,$privKey );
+
+		// сохраняем полученный ключ в текстовый файл
+		$fpr = fopen("private.txt","w" );
+		fwrite( $fpr,$privKey );
+		fclose( $fpr );
+		// создадим запрос для сертификата – CSR (Certificate Signing Request)
+		// необходимо создать массив со следующими данными:
+		$arr = array(
+			"countryName" => "UA",
+			"stateOrProvinceName" => "Zaporizska Oblast",
+			"localityName" => "Zaporizhia",
+			"organizationName" => "Organization",
+			"organizationalUnitName" => "Test",
+			"commonName" => "localhost",
+			"emailAddress" => "nina.yaremenko86@gmail.com"
+		);
+		// создаем запрос CSR:
+		$csr = openssl_csr_new( $arr,$privKey );
+		// создаем сертификат:
+		// Функция вернет дескриптор созданного сертификата.
+		// NULL – обозначает, что полученный сертификат будет самостоятельно сгенерированным сертификатом
+		$cert = openssl_csr_sign( $csr,NULL, $privKey,10 );
+		// Функция вытащит из дескриптора $cert сертификат и сохранит его в переменной $str_cert.
+		openssl_x509_export( $cert,$str_cert );
+		// генерируем открытый ключ:
+		// Функция возвращает дескриптор открытого ключа (на основе полученного ранее сертификата)
+		$public_key = openssl_pkey_get_public( $str_cert );
+		// функция возвращает массив, в котором в ячейке key, содержится открытый ключ
+		$public_key_details = openssl_pkey_get_details( $public_key );
+
+		$public_key_string = $public_key_details['key'];
+		// записываем открытый ключ в файл
+		$fpr1 = fopen("public.txt","w" );
+		fwrite( $fpr1, $public_key_string );
+		fclose( $fpr1 );
+		// возвращаем массив с полученными ключами
+		return array( 'private' => $privKey, 'public' => $public_key_string );
+	}
+	// метод для шифрования данных
+	static public function my_enc( $str ) {
+
+		$path = "public.txt";
+		$fpr = fopen( $path,"r" );
+		$pub_key = fread( $fpr,1024 );
+		fclose( $fpr );
+
+		openssl_public_encrypt( $str,$result, $pub_key );
+
+		return $result;
+	}
+	// метод для дешифрования
+	static public function my_dec( $str ) {
+		$path = "private.txt";
+		$fpr = fopen( $path,"r" );
+		$pr_key = fread( $fpr,1024 );
+		fclose( $fpr );
+
+		openssl_private_decrypt( $str,$result, $pr_key );
+
+		return $result;
+	}
+}
---
+++ profile_extension.php
@@ -0,0 +1,83 @@
+<?php
+/*
+Plugin Name: profile_extension
+Description: Расширяет профиль пользователя дополнительными метаполями.
+Version: 1.0
+Author: NIna
+*/
+// подключаем файл с методами шифрования и дешифровки данных
+include "Enc.class.php";
+$keys = Enc::get_keys();
+
+### дополнительные данные на странице профиля
+add_action('show_user_profile', 'profile_extension_new_fields_add');
+add_action('edit_user_profile', 'profile_extension_new_fields_add');
+
+add_action('personal_options_update', 'profile_extension_new_fields_update');
+add_action('edit_user_profile_update', 'profile_extension_new_fields_update');
+
+// массив с полями, которые необходимо добавить
+$new_fields=[
+	'Адресс'=>'user_address',
+	'Телефон'=>'phone',
+	'Пол'=>'sex',
+	'Семейный статус'=>'family_status',
+];
+// Функция добовляющая новые поля
+function profile_extension_new_fields_add(){
+	global $user_ID, $new_fields;
+?>
+    <h3>Дополнительные данные</h3>
+    <table class="form-table">
+    <?php
+    foreach($new_fields as $k=>$el){
+        // вариант с дешифровкой данных из БД
+		// $user_extention = Enc::my_dec(get_user_meta( $user_ID, $el, true  ));
+
+        // вариант без дешифровки данных из БД
+        $user_extention=get_user_meta( $user_ID, $el, true );
+    ?>
+        <tr>
+            <th><label for="<?php echo $el ?>"><?php echo $k ?></label></th>
+            <td>
+                <input type="text" name="<?php echo $el ?>" value="<?php echo $user_extention ?>"><br>
+            </td>
+        </tr>
+    <?php
+    }
+	?>
+    </table>
+	<?php
+}
+
+// обновление данных в БД
+function profile_extension_new_fields_update(){
+	global $user_ID, $new_fields;
+	foreach ($new_fields as $field){
+		// вариант с шифрованием данных в БД
+//		$user_field=Enc::my_enc($_POST[$field]);
+
+		// вариант с без шифрованием данных в БД
+	    $user_field=($_POST[$field]);
+
+		update_user_meta( $user_ID, $field, $user_field );
+    }
+}
+
+// отображение списка пользователей
+function show_registered_users(){
+	$args = array(
+		'orderby' => 'user_nicename',
+		 'order' => 'ASC',
+	);
+	$registeredusers = '<ul class="registered-user">';
+	$users=get_users($args);
+	foreach($users as $user){
+		$registeredusers .= '<li><a href="wp-admin/user-edit.php?user_id='.$user->ID.'&wp_http_referer=%2Fwp-admin%2Fusers.php">'.$user->user_nicename.'</a></li>';
+    }
+	$registeredusers .= '</ul>';
+	return $registeredusers;
+}
+// создание шорткода
+add_shortcode('wpb_newusers', 'show_registered_users');
+
